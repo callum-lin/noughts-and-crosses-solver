59edac503630d22ad06cfc78e7c38b8f0b84e76e
Callum Lin <calluml1225@gmail.com>
2025-04-10 18:10:03 +0100

    mvp

---
+++ noughts_and_crosses.py
@@ -0,0 +1,208 @@
+from math import inf
+import signal
+from sys import exit
+from string import ascii_uppercase
+from time import sleep
+
+
+def get_all_child_moves(board):
+    for row in range(3):
+        for column in range(3):
+            if board[row][column] == " ":
+                yield row, column
+
+
+def crosses_to_move(board):
+    noughts = 0
+    crosses = 0
+    for i in board:
+        for j in i:
+            match j:
+                case "O":
+                    noughts += 1
+                case "X":
+                    crosses += 1
+    return crosses == noughts
+    """Note that the above is significantly faster than doing
+    noughts = sum(row.count("O") for row in board)
+    crosses = sum(row.count("X") for row in board)
+    """
+
+
+def is_there_three_in_a_row(board, symbol):
+    for row in board:
+        if row[0] == row[1] == row[2] == symbol:
+            return True
+    for column in range(len(board)):
+        if board[0][column] == board[1][column] == board[2][column] == symbol:
+            return True
+    if board[0][0] == board[1][1] == board[2][2] == symbol:
+        return True
+    if board[0][2] == board[1][1] == board[2][0] == symbol:
+        return True
+    return False
+
+
+def have_noughts_won(board):
+    return is_there_three_in_a_row(board, "O")
+
+
+def have_crosses_won(board):
+    return is_there_three_in_a_row(board, "X")
+
+
+def is_draw(board):
+    if have_crosses_won(board) or have_noughts_won(board):
+        return False
+    for row in board:
+        if " " in row:
+            return False
+    return True
+
+
+def game_ended(board):
+    if have_crosses_won(board) or have_noughts_won(board) or is_draw(board):
+        return True
+    return False
+
+
+def evaluate(board):
+    multiplier = 1 if crosses_to_move(board) else -1
+    if game_ended(board):
+        if have_crosses_won(board):
+            return 1 * multiplier
+        elif have_noughts_won(board):
+            return -1 * multiplier
+        else:
+            return 0
+    raise TypeError("Only call this function when the game has ended.")
+
+
+def make_move(board, move):
+    side_to_move = "X" if crosses_to_move(board) else "O"
+    move_row, move_column = move
+    board[move_row][move_column] = side_to_move
+
+
+def unmake_move(board, move):
+    move_row, move_column = move
+    board[move_row][move_column] = " "
+
+
+def negamax(board, starting=True):
+    if game_ended(board):
+        return evaluate(board)
+    side_to_move = "X" if crosses_to_move(board) else "O"
+    max_score = -inf
+    best_move = None
+    for move in get_all_child_moves(board):
+        make_move(board, move)
+        score = -negamax(board, starting=False)
+        unmake_move(board, move)
+        if score > max_score:
+            max_score = score
+            best_move = move
+    if starting:
+        return best_move
+    return max_score
+
+
+
+
+def is_move_valid(board, move):
+    return board[move[0]][move[1]] == " "
+
+
+def clear_screen():
+    print("\x1Bc")
+
+
+def print_board(board):
+    print("     A   B   C")
+    print("    -----------")
+    for i, row in enumerate(board):
+        print(i + 1, " |", " | ".join(row), "|")
+    print("    -----------")
+
+
+def user_input_helper():
+    user_input = input("Enter a move or type EXIT to exit: ")
+    user_input = user_input.upper()
+    if user_input == "EXIT":
+        clear_screen()
+        disable_alternate_text_buffer()
+        exit()
+    move_column = ascii_uppercase.find(user_input[0])
+    move_row = int(user_input[1]) - 1
+    return (move_row, move_column)
+
+
+def get_user_input(board):
+    while True:
+        move = user_input_helper()
+        if is_move_valid(board, move):
+            return move
+        print("That was an invalid move")
+        sleep(2)
+        clear_screen()
+        print_board(board)
+
+
+def game_loop(board):
+    print_board(board)
+    print("The AI is thinking...")
+    while not game_ended(board):
+        best_move = negamax(board)
+        make_move(board, best_move)
+        clear_screen()
+        print_board(board)
+        if game_ended(board):
+            return
+        user_move = get_user_input(board)
+        make_move(board, user_move)
+        clear_screen()
+        print_board(board)
+        print("The AI is thinking...")
+
+
+def show_message(board):
+    if not game_ended:
+        print("The game is still ongoing.")
+        return
+    if have_crosses_won(board):
+        print("Crosses have won!")
+    elif have_noughts_won(board):
+        print("Noughts have won!")
+    else:
+        print("The game ended in a draw.")
+
+
+def enable_alternate_text_buffer():
+    print("\x1B[?1049h")
+
+
+def disable_alternate_text_buffer():
+    print("\x1B[?1049l")
+
+
+def main():
+    board = [[" ", " ", " "] for i in range(3)]
+    enable_alternate_text_buffer()
+    game_loop(board)
+    show_message(board)
+    sleep(4)
+    clear_screen()
+    disable_alternate_text_buffer()
+
+
+def signal_handler(sig, frame):
+    print("Program shutting down.")
+    print("Restoring original environment...")
+    clear_screen()
+    disable_alternate_text_buffer()
+    exit()
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    main()
